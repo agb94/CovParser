dc7eb8b22b7f24374daa8a930c0952021c199906
Gabin An <agb94@kaist.ac.kr>
2020-03-02 23:07:20 +0900

    Return function range (gcov)

--- cov_parser/gcov.py
+++ cov_parser/gcov.py
@@ -1,9 +1,18 @@
-def gcov(path):
+def gcov(path, f_lines=None):
     hits = {}
     with open(path, 'r') as f:
         for line in f:
+            if line.startswith("function"):
+                if f_lines is not None:
+                    function = line.split()[1]
+                    f_lines[function] = []
+                continue
+            elif line.startswith("branch"):
+                continue
             count, number = line.split(':')[0].strip(), int(line.split(':')[1].strip())
             if number < 1 or count == '-':
                 continue
+            if f_lines is not None and function is not None:
+                f_lines[function].append(number)
             hits[number] = 0 if count=='#####' else int(count)
     return hits
